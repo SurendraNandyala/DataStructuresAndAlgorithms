2aa7a998c7367d9f80fa7ce8eefac7e8384020dd
Surendra Nandyala <snandyala@extremenetworks.com>
2020-03-03 17:04:15 +0530

    Restaurant-Service, stage-1.1

--- online-food-delivery-service/restaurant-service/src/main/java/com/surendra/service/impl/MenuItemServiceImpl.java
+++ online-food-delivery-service/restaurant-service/src/main/java/com/surendra/service/impl/MenuItemServiceImpl.java
@@ -1,10 +1,12 @@
 package com.surendra.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.surendra.repository.MenuItemRepository;
 import com.surendra.service.MenuItemService;
 
+@Service
 public class MenuItemServiceImpl implements MenuItemService {
 
 	private MenuItemRepository menuItemRepository;
--- online-food-delivery-service/restaurant-service/src/main/java/com/surendra/service/impl/RestaurantServiceImpl.java
+++ online-food-delivery-service/restaurant-service/src/main/java/com/surendra/service/impl/RestaurantServiceImpl.java
@@ -1,10 +1,12 @@
 package com.surendra.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.surendra.repository.RestaurantRepository;
 import com.surendra.service.RestaurantService;
 
+@Service
 public class RestaurantServiceImpl implements RestaurantService{
 
 	private RestaurantRepository restaurantRepository;
